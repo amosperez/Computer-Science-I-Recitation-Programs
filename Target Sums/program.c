75778274519d107cd2407660c29f62c8f72b60a8
amosperez <amos7perez@gmail.com>
2024-07-09 22:49:39 -0400

    Assignment in Progress

--- Target Sums/program.c
+++ Target Sums/program.c
@@ -2,3 +2,26 @@
 // 07/09/2024
 // Target Sums
 // Arup Gupta's Computer Science I Recitation Programs
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *openFile(char filepath[], char filemode[]);
+
+int main()
+{
+    FILE *sample_in = openFile("./sample-test.in", "r");
+    return 0;
+}
+
+FILE *openFile(char filepath[], char filemode[])
+{
+    FILE *file = fopen(filepath, filemode);
+    if (file != NULL)
+        printf("\"%s\" is open in \"%s\" mode.\n", filepath, filemode);
+    else
+    {
+        printf("\"%s\" failed to open.\n", filepath);
+        exit(1);
+    }
+    return file;
+}
